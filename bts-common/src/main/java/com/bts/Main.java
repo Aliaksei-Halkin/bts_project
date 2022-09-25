6f451f9a6440ea6592ede6282c51d6d1ab48d0dc
Aliaksei Halkin <pochtax3@gmail.com>
2022-09-25 12:12:56 +0300

    added scileton

---
+++ bts-common/src/main/java/com/bts/Main.java
@@ -0,0 +1,7 @@
+package com.bts;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ bts-web/src/main/java/com/bts/recruiting/RecruitingApplication.java
@@ -0,0 +1,21 @@
+package com.bts.recruiting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
+
+@PropertySources(value = {
+        @PropertySource(value = "classpath:application.properties"),
+        @PropertySource(value = "classpath:application-${spring.profiles.active}.properties")
+})
+@ConfigurationPropertiesScan("com.bts.recruiting")
+@SpringBootApplication
+public class RecruitingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RecruitingApplication.class, args);
+    }
+
+}
